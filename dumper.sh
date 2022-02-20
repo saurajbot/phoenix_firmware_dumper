13fe67b391d58e577e16b5093a1627292c688bdf
noobyysauraj <gitsauraj@gmail.com>
2022-02-20 16:51:24 +0530

    Doing anything

--- dumper.sh
+++ dumper.sh
@@ -1164,10 +1164,10 @@ if [[ -s "${PROJECT_DIR}"/.gitlab_token ]]; then
 
 		} >> "${OUTDIR}"/tg.html
 		TEXT=$(< "${OUTDIR}"/tg.html)
-		mv "${OUTDIR}"/tg.html /home/runner/telegram/tg.html
+		mv "${OUTDIR}"/tg.html /home/runner/work/dumper/dumper/telegram/tg.html
 		curl -s "https://api.telegram.org/bot${TG_TOKEN}/editMessageText" --data "message_id=${M_ID}&text=${TEXT}&chat_id=${C_ID}&parse_mode=HTML&disable_web_page_preview=True" || printf "Telegram Notification Sending Error.\n"
 		curl -s "https://api.telegram.org/bot${TG_TOKEN}/editMessageText" --data "message_id=${MESSAGE_ID}&text=${TEXT}&chat_id=${CHAT_ID}&parse_mode=HTML&disable_web_page_preview=True" || printf "Telegram Notification Sending Error.\n" # Send Edited Message To The Telegram Group In Which /dump Command Is Initiated
-                mv "${OUTDIR}" /home/runner/dump
+                mv "${OUTDIR}" /home/runner/work/dumper/dumper/dumped
                 ls /home/runner
                 ls /home/runner/dump
                 cat /home/runner/telegram/chat_id
